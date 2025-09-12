75b15690d1f1fe3001ab97863c99ed6c74eb97d5
Zomblack3 <santi260406@gmail.com>
2025-09-12 12:09:12 -0300

    gameplay archives and basic functions added
    gameplay.cpp & gameplay.h are added and the base, update and draw functions were created

---
+++ src/gameplay.cpp
@@ -0,0 +1,18 @@
+#include "gameplay.h"
+
+void gameplay(SCREENS& actualScreen)
+{
+	updateGameplay(actualScreen);
+	drawGameplay();
+}
+
+void updateGameplay(SCREENS& actualScreen)
+{
+
+}
+
+void drawGameplay()
+{
+
+}
+
---
+++ src/gameplay.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "gameManager.h"
+
+void gameplay(SCREENS& actualScreen);
+
+void updateGameplay(SCREENS& actualScreen);
+void drawGameplay();
