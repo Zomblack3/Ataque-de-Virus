6eebc4006c7d0f616374f76cb4355c097d9dbdeb
Zomblack3 <santi260406@gmail.com>
2025-09-12 11:57:46 -0300

    window and game screens setted
    the game window and the basic screens were setted

--- src/game.cpp
+++ src/game.cpp
@@ -6,6 +6,37 @@ namespace run
 {
 	void game()
 	{
-		
+		SCREENS actualScreen = MAIN_MENU;
+
+		slWindow(windowWidth, windowHeight, "BREAKOUT", 0);
+
+		while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+		{
+			switch (actualScreen)
+			{
+			case MAIN_MENU:
+
+
+
+				break;
+			case OPTIONS:
+				
+				
+				
+				break;
+			case GAMEPLAY:
+				
+				
+				
+				break;
+			case EXIT:
+				
+				
+				
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
