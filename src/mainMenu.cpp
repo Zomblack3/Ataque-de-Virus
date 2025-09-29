a8e95095093388873352e9b0f84ed9277ad690fc
Zomblack3 <santi260406@gmail.com>
2025-09-29 01:15:06 -0300

    control instructions added

--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -2,6 +2,8 @@
 
 #include "sl.h"
 
+bool areTheControlesShown = false;
+
 void mainMenu(SCREENS& actualScreen)
 {
 	updateMainMenu(actualScreen);
@@ -11,8 +13,19 @@ void mainMenu(SCREENS& actualScreen)
 
 void updateMainMenu(SCREENS& actualScreen)
 {
-	if (slGetKey('i') || slGetKey('I'))
-		actualScreen = GAMEPLAY;
+	if (!areTheControlesShown)
+	{
+		if (slGetKey('i') || slGetKey('I'))
+			actualScreen = GAMEPLAY;
+
+		if (slGetKey('c') || slGetKey('C'))
+			areTheControlesShown = true;
+	}
+	else
+	{
+		if (slGetKey('v') || slGetKey('V'))
+			areTheControlesShown = false;
+	}
 }
 
 void drawMainMenu()
@@ -21,10 +34,22 @@ void drawMainMenu()
 
 	slSetForeColor(1, 1, 1, 100);
 
-	slText(windowWidth / 2.0f - (6.0f * 24.0f), (windowHeight / 2.0f) + 200, "BREAKOUT PROJECT");
-	slText(windowWidth / 2.0f - (10.5f * 24.0f), windowHeight / 2.0f, "Presione la tecla I para iniciar");
-	slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 50, "Presione la tecla S para salir");
-	slText(10, 10, "Hecho por Zomblack3 (Santiago Britos)");
+	if (!areTheControlesShown)
+	{
+		slText(windowWidth / 2.0f - (6.0f * 24.0f), (windowHeight / 2.0f) + 200, "BREAKOUT PROJECT");
+		slText(windowWidth / 2.0f - (10.5f * 24.0f), windowHeight / 2.0f, "Presione la tecla I para iniciar");
+		slText(windowWidth / 2.0f - (13.0f * 24.0f), (windowHeight / 2.0f) - 50, "Presione la tecla C para ver los controles");
+		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 100, "Presione la tecla S para salir");
+		slText(10, 10, "Hecho por Zomblack3 (Santiago Britos)");
+	}
+	else
+	{
+		slText(windowWidth / 2.0f - (14.0f * 24.0f), (windowHeight / 2.0f) + 50, "Moverse (izquierda): A / Flecha izquierda");
+		slText(windowWidth / 2.0f - (12.0f * 24.0f), windowHeight / 2.0f, "Moverse (derecha): D / Flecha derecha");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), (windowHeight / 2.0f) - 50, "Iniciar partida; Barra espaciadora");
+		slText(windowWidth / 2.0f - (3.0f * 24.0f), (windowHeight / 2.0f) - 100, "Pausa: P");
+		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 150, "Presione V para volver al menu");
+	}
 
 	slRender();
 }
