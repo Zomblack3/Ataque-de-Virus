4c36fb0ccfc62085588ac2bf45f8e9b31d613eb2
Zomblack3 <santi260406@gmail.com>
2025-09-29 01:37:07 -0300

    rules added

--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -3,6 +3,7 @@
 #include "sl.h"
 
 bool areTheControlesShown = false;
+bool areTheRulesShown = false;
 
 void mainMenu(SCREENS& actualScreen)
 {
@@ -13,18 +14,27 @@ void mainMenu(SCREENS& actualScreen)
 
 void updateMainMenu(SCREENS& actualScreen)
 {
-	if (!areTheControlesShown)
+	if (!areTheControlesShown && !areTheRulesShown)
 	{
 		if (slGetKey('i') || slGetKey('I'))
 			actualScreen = GAMEPLAY;
 
 		if (slGetKey('c') || slGetKey('C'))
 			areTheControlesShown = true;
+
+		if (slGetKey('r') || slGetKey('R'))
+			areTheRulesShown = true;
 	}
 	else
 	{
 		if (slGetKey('v') || slGetKey('V'))
-			areTheControlesShown = false;
+		{
+			if (areTheControlesShown)
+				areTheControlesShown = false;
+
+			if (areTheRulesShown)
+				areTheRulesShown = false;
+		}
 	}
 }
 
@@ -34,15 +44,16 @@ void drawMainMenu()
 
 	slSetForeColor(1, 1, 1, 100);
 
-	if (!areTheControlesShown)
+	if (!areTheControlesShown && !areTheRulesShown)
 	{
 		slText(windowWidth / 2.0f - (6.0f * 24.0f), (windowHeight / 2.0f) + 200, "BREAKOUT PROJECT");
 		slText(windowWidth / 2.0f - (10.5f * 24.0f), windowHeight / 2.0f, "Presione la tecla I para iniciar");
 		slText(windowWidth / 2.0f - (13.0f * 24.0f), (windowHeight / 2.0f) - 50, "Presione la tecla C para ver los controles");
-		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 100, "Presione la tecla S para salir");
+		slText(windowWidth / 2.0f - (13.0f * 24.0f), (windowHeight / 2.0f) - 100, "Presione la tecla R para ver las reglas");
+		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 150, "Presione la tecla S para salir");
 		slText(10, 10, "Hecho por Zomblack3 (Santiago Britos)");
 	}
-	else
+	else if (areTheControlesShown)
 	{
 		slText(windowWidth / 2.0f - (14.0f * 24.0f), (windowHeight / 2.0f) + 50, "Moverse (izquierda): A / Flecha izquierda");
 		slText(windowWidth / 2.0f - (12.0f * 24.0f), windowHeight / 2.0f, "Moverse (derecha): D / Flecha derecha");
@@ -50,6 +61,20 @@ void drawMainMenu()
 		slText(windowWidth / 2.0f - (3.0f * 24.0f), (windowHeight / 2.0f) - 100, "Pausa: P");
 		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 150, "Presione V para volver al menu");
 	}
+	else
+	{
+		slText(windowWidth / 2.0f - (9.0f * 24.0f), (windowHeight / 2.0f) + 150, "El jugador debera eliminar a");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), (windowHeight / 2.0f) + 100, "todos los ladrillos de la pantalla");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), (windowHeight / 2.0f) + 50, "haciendo rebotar una bola en el,");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), windowHeight / 2.0f, "cada vez que la bola rebote contra");
+		slText(windowWidth / 2.0f - (12.0f * 24.0f), (windowHeight / 2.0f) - 50, "los ladrillos estos seran eliminados.");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), (windowHeight / 2.0f) - 100, "En caso de que la pelota salga de");
+		slText(windowWidth / 2.0f - (11.0f * 24.0f), (windowHeight / 2.0f) - 150, "la pantalla por el lado inferior");
+		slText(windowWidth / 2.0f - (9.0f * 24.0f), (windowHeight / 2.0f) - 200, "el jugador perdera una vida,");
+		slText(windowWidth / 2.0f - (13.0f * 24.0f), (windowHeight / 2.0f) - 250, "si pierde tres vidas pierde la partida");
+		slText(windowWidth / 2.0f - (10.0f * 24.0f), (windowHeight / 2.0f) - 300, "Presione V para volver al menu");
+	}
+
 
 	slRender();
 }
